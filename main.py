fe7a4ba6b6ccc9a2bfad9f4eb722fe89b0049e9c
Nikita <60404032+nglevc39@users.noreply.github.com>
2020-02-13 14:34:07 -0500

    Programming Game
    asdasda

--- main.py
+++ main.py
@@ -1,17 +1,32 @@
 import pyglet # import the library
-win= pyglet.window.Window() # create the window
+win = pyglet.window.Window() # create the window
+keys = pyglet.window.key.KeyStateHandler()
+# Load the image
+img = pyglet.image.load('assets/0/sliced/idle-1.png')
+spr = pyglet.sprite.Sprite(img, x = 300, y = 250)
 
-# Create a sprite
-img= pyglet.image.load('Nature.jpg')
-
-def update(dt):
-  pass
+# spr = pyglet.sprite.Sprite(img, x=50, y=50)
 
 # Start the event loop
+keys = pyglet.window.key.KeyStateHandler()
+
+def update(dt):
+    win.push_handlers(keys)             # update the keys object
+    if keys[pyglet.window.key.LEFT]: 
+        spr.x -= 2 
+    if keys[pyglet.window.key.RIGHT]: 
+        spr.x += 2 
+    if keys[pyglet.window.key.DOWN]: 
+        spr.y -= 2  
+    if keys[pyglet.window.key.UP]: 
+        spr.y += 2                             # change spr.x by -1
+
+
 @win.event
 def on_draw():
-    win.clear()
-    img.blit(100, 150)
+  win.clear()
+  # img.blit(200, 100)
+  spr.draw()
 
-pyglet.clock.schedule(update) 
+pyglet.clock.schedule(update)
 pyglet.app.run()
